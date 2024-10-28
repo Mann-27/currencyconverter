b5cbd8d4a4df5496b2de7318f81b4dc6a439272d
Mann Merwana <mann.merwana27@gmail.com>
2024-10-28 11:07:09 +0530

    congif changed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/currencyconverter",
+  base: "https://github.com/Mann-27/currencyconverter",
   plugins: [react()],
 });
